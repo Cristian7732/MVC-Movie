d32886fd9bd69002de7e48690bee1ef50802c293
Cristian7732 <cristianadolfo484@gmail.com>
2022-06-14 21:32:15 -0400

    Agregar archivos de proyecto.

---
+++ MVC Movie/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Movie.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Release_Date { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
